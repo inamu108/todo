107956dfefbbe92b211bb5778b18a4ed551a84af
inamu108 <inamublog@gmail.com>
2020-06-19 12:45:01 +0900

    Update Board.vue

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,6 +3,7 @@
     <header>my Trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <list v-for="(list, index) in lists" :key="list.id" :title="list.title" :listIndex="index"></list>
       <ListAdd></ListAdd>
     </main>
   </div>
@@ -10,10 +11,18 @@
 
 <script>
 import ListAdd from './ListAdd.vue'
+import List from './List.vue'
+import { mapState} from 'vuex'
 
 export default {
   components: {
     ListAdd,
+    List,
+  },
+  computed: {
+    ...mapState([
+      'lists'
+    ]),
   },
 }
 </script>
