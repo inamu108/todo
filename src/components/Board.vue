dd3681601b23378c7d7c342373228337298b7041
inamu108 <inamublog@gmail.com>
2020-06-19 11:04:19 +0900

    initial-commit

---
+++ src/components/Board.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>my Trello</header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+      <ListAdd></ListAdd>
+    </main>
+  </div>
+</template>
+
+<script>
+import ListAdd from './ListAdd.vue'
+
+export default {
+  components: {
+    ListAdd,
+  },
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,55 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+//trello-listsをlocalStorageから取得する
+const savedLists = localStorage.getItem('trello-lists')
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
+    //localStorageに保存されたリストがあれば取得、なければデフォルトのリスト配列を設置します。
+    //localStorageにはJSON形式の文字列型でデータが保存されているので、
+    //取得するときにはJSON.parse(取得するデータ)でオブジェクトに変換する必要があります。
+    lists: savedLists ? JSON.parse(savedLists):[
+      {
+        title: 'Backlog',
+        cards: [
+          { body: 'English'},
+          { body: 'Mathematics'},
+        ]
+      },
+      {
+        title: 'Todo',
+        cards: [
+          { body: 'Science'}
+        ]
+      },
+      {
+        title: 'Doing',
+        cards: []
+      }
+    ],
   },
   mutations: {
+    addlist(state, payload) {
+      state.lists.push({title: payload.title, card:[]})
+    },
   },
   actions: {
+    addlist(context, payload) {
+      context.commit('addlist', payload)
+    },
   },
   modules: {
+  },
+  getters: {
   }
 })
+//下記のコードで、データの状態を更新後にlocalStorageへデータの状態を保存しています。
+//stringifyでオブジェクトのtrello-listsをjson文字列に変換して保存
+store.subscribe((mutation, state) => {
+  localStorage.setItem('trello-lists', JSON.stringify(state.lists))
+})
+//trello-listsをjson文字列に変換
+
+
+export default store
